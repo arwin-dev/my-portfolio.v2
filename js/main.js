802a79a3983b2a732a044cae0f790760567101b3
arwin.dev <arwinswapna@usf.edu>
2022-04-16 00:55:38 -0400

    about completed, scroll animation added

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//Navigation bar effect on scroll
+
+window.addEventListener("scroll",function()
+{
+    const header = this.document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
