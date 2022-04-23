2b3d8cf6d8230851f168670cd90cb87c4f0bad6c
arwin.dev <arwinswapna@usf.edu>
2022-04-23 14:20:16 -0400

    exp section completed

--- js/main.js
+++ js/main.js
@@ -5,3 +5,27 @@ window.addEventListener("scroll",function()
     const header = this.document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+//Experience Card 
+
+const experienceModels = document.querySelectorAll(".work-card-content");
+const learnMoreBtn = document.querySelectorAll(".learnMoreBtn");
+const closeCardBtn = document.querySelectorAll(".work-card-closebtn")
+
+var model = function(modalCLick){
+    experienceModels[modalCLick].classList.add("active");
+}
+
+learnMoreBtn.forEach((learnMoreBtn, i) => {
+    learnMoreBtn.addEventListener("click", () => {
+        model(i);
+    });
+});
+
+closeCardBtn.forEach((closeCardBtn) => {
+    closeCardBtn.addEventListener("click", () => {
+        experienceModels.forEach((modalView) => {
+            modalView.classList.remove("active");
+        });
+    });
+});
